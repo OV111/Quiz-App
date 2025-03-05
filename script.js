1273d52a057d0b19558a9be06ea0e8910cf3198c
Vahe <ohanyanv703@gmail.com>
2025-03-05 23:17:31 +0400

    adding obj js

---
+++ script.js
@@ -0,0 +1,23 @@
+const questions = [
+    {   question: "Question Which data structure follows the First In, First Out (FIFO) principle?",
+        answers: [
+            {text:"Stack",correct: true},
+            {text:"Queue",correct: false},
+            {text:"Linked List",correct: false},
+            {text:"Hash Table",correct: false},
+        ],
+    },
+    {
+        
+        question: "Which programming language is primarily used for building websites?",
+        answers: [
+            {text: "Python", correct: false},
+            {text: "JavaScript", correct: true},
+            {text: "C++",correct: false},
+            {text: "Java",correct: false},
+        ],
+    },
+    {
+
+    },
+];
